c27c249a13f330989964ff9006cfe2b34576394a
Aiba <aiba.prenov@gmail.com>
2018-11-20 22:16:14 +0600

    ADD: html

--- nakey/settings.py
+++ nakey/settings.py
@@ -102,7 +102,7 @@ class BaseConfiguration(Configuration):
     USE_TZ = True
 
     STATIC_URL = '/static/'
-    STATIC_ROOT = "/static"
+    STATIC_ROOT = '/static'
     STATICFILES_DIRS = (os.path.join(PROJECT_DIR, "static"),)
 
     MEDIA_URL = '/media/'
