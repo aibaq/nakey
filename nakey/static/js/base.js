65703e84a81eca3dc095f1b37dd867aebe50ef6c
kerey <akzholtay.k@gmail.com>
2019-02-11 02:56:59 +0600

    logs --

--- nakey/static/js/base.js
+++ nakey/static/js/base.js
@@ -18,7 +18,7 @@ function onAddToCart(item){
     item.count = 1;//default count is 1;
     if(localStorage.getItem("cartItems")){
         items = JSON.parse(localStorage.getItem("cartItems"));
-        console.log(items.find(i=>i.id == item.id))
+        // console.log(items.find(i=>i.id == item.id))
         if(items.find(i=>i.id == item.id) == undefined){
             items.push(item);
             toastr.success('Товар добавлен в корзину')
@@ -32,20 +32,20 @@ function onAddToCart(item){
     onUpdateCartItems(items)
 }
 function onRemoveFromCart(item_id){
-    console.log(item_id)
+    // console.log(item_id)
     var items = onGetCartItems();
     var index = items.findIndex(item=>item.id == item_id);
     if(index > -1){
         items.splice(index, 1);
         onUpdateCartItems(items)
     }
-    console.log(items);
+    // console.log(items);
 }
 function onUpdateCartItem(item_id, newcount){
     items = onGetCartItems();
-    console.log("Update", items, item_id, newcount)
+    // console.log("Update", items, item_id, newcount)
     var searchitem = items.find(item=>{ return item.id == item_id; });
-    console.log("search", searchitem)
+    // console.log("search", searchitem)
     if(searchitem != undefined){
         searchitem.count = newcount;
         onUpdateCartItems(items)
@@ -87,6 +87,6 @@ function setUrlVar(query){
         let before = i==0?'?':'&';
         str+=before+vars[i]+'='+vars[vars[i]];
     }
-    console.log('str:', vars);
+    // console.log('str:', vars);
     return str;
 }
